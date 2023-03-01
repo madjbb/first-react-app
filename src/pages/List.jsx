3869563876d29df5b640065692d0e5ebc32113ef
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-03-01 19:32:41 +0000

    added MUI and theming

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function List() {
   return (
     <>
       <div>List</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
-  )
+  );
 }
 
-export default List
+export default List;
