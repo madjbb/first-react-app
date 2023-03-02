bedb97ea23265afb6419d137494dc449b4eadc09
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-03-02 16:10:55 +0000

    update and delete functionality wired to form

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Typography from '@mui/material/Typography';
 import CarForm from '../components/forms/CarForm';
 
+import { CarsContext } from '../components/contexts/car.context';
+
 function Add() {
+  const {addCar} = useContext(CarsContext);
   return (
     <>
       <Typography variant="h2" component="h1">
         Add Car
       </Typography>
-      <CarForm />
+      <CarForm submitHandler={addCar} />
     </>
   );
 }
