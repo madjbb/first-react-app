64151db2549c5c1331584f3d7205f4ea3df880d8
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-03-02 13:01:39 +0000

    forms added

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import CarForm from '../components/forms/CarForm';
 
 function Add() {
   return (
     <>
-      <div>Add</div>
+      <Typography variant="h2" component="h1">
+        Add Car
+      </Typography>
+      <CarForm />
     </>
-  )
+  );
 }
 
-export default Add
+export default Add;
